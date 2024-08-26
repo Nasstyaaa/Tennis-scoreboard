d230f3bf9bb0a9f4fcd05fdd81750e0502e78d5e
Nasstyaaa <Luk-2004@mail.ru>
2024-08-26 14:04:38 +0300

    feat:create GameCalculation extended from Calculation

--- src/main/java/org/nastya/chaincalculation/GameCalculation.java
+++ src/main/java/org/nastya/chaincalculation/GameCalculation.java
@@ -2,18 +2,16 @@ package org.nastya.chaincalculation;
 
 import org.nastya.model.Score;
 
-public class GameCalculation extends Calculation{
+public class GameCalculation extends Calculation {
 
     @Override
     public void calculate(Score score, Score scoreOpponent) {
+        score.setGame(score.getGame() + 1);
+        score.setPoint(0);
+        scoreOpponent.setPoint(0);
+
         if (score.getGame() >= 6) {
-            if (scoreOpponent.getGame() < 5 || score.getGame() == 7) {
-                score.setSet(score.getSet() + 1);
-                score.setGame(0);
-                scoreOpponent.setGame(0);
-            } else if (scoreOpponent.getGame() == 6) {
-                nextCalculation.calculate(score, scoreOpponent);
-            }
+            nextCalculation.calculate(score, scoreOpponent);
         }
     }
 }
--- src/main/java/org/nastya/service/MatchScoreCalculationService.java
+++ src/main/java/org/nastya/service/MatchScoreCalculationService.java
@@ -4,48 +4,5 @@ import org.nastya.model.Score;
 
 
 public class MatchScoreCalculationService {
-
-    public void compute(Score score, Score scoreOpponent) {
-        playGame(score, scoreOpponent);
-        playSet(score, scoreOpponent);
-    }
-
-    private void playSet(Score score, Score scoreOpponent) {
-        if (score.getGame() >= 6) {
-            if (scoreOpponent.getGame() < 5 || score.getGame() == 7) {
-                score.setSet(score.getSet() + 1);
-                score.setGame(0);
-                scoreOpponent.setGame(0);
-            } else if (scoreOpponent.getGame() == 6) {
-                playTieBreak(score, scoreOpponent);
-            }
-        }
-    }
-
-    private void playGame(Score score, Score scoreOpponent) {
-        switch (score.getPoint()) {
-            case 0:
-                score.setPoint(15);
-                break;
-            case 15:
-                score.setPoint(30);
-                break;
-            case 30:
-                score.setPoint(40);
-            case 40:
-                if (scoreOpponent.getPoint() == 40) {
-                    break; //TODOЕсли оба игрока имеют по 40, то выигрыш следующей подачи дает преимущество.
-                    // Игрок, имеющий преимущество и выигравший следующую подачу, побеждает в гейме.
-                }
-                score.setPoint(0);
-                scoreOpponent.setPoint(0);
-                score.setGame(score.getGame() + 1);
-                break;
-        }
-    }
-
-    private void playTieBreak(Score score, Score scoreOpponent) {
-        //TODO прописать
-    }
 }
 
